7f8fcca3f7bbd1328ce8cfbcfee8e0bb2ef090d2
Kairi <kadar.marcell69@gmail.com>
2022-02-21 22:55:24 +0100

    Well stuff happened

---
+++ R5R_Installer/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace R5R_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.downRate = new System.Windows.Forms.TextBox();
+            this.downRateAvg = new System.Windows.Forms.TextBox();
+            this.eta = new System.Windows.Forms.TextBox();
+            this.dpeers = new System.Windows.Forms.TextBox();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.discordLink = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Download Directory";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(211, 317);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(339, 22);
+            this.output.TabIndex = 4;
+            // 
+            // downRate
+            // 
+            this.downRate.Location = new System.Drawing.Point(639, 127);
+            this.downRate.Name = "downRate";
+            this.downRate.Size = new System.Drawing.Size(100, 22);
+            this.downRate.TabIndex = 5;
+            // 
+            // downRateAvg
+            // 
+            this.downRateAvg.Location = new System.Drawing.Point(639, 164);
+            this.downRateAvg.Name = "downRateAvg";
+            this.downRateAvg.Size = new System.Drawing.Size(100, 22);
+            this.downRateAvg.TabIndex = 6;
+            // 
+            // eta
+            // 
+            this.eta.Location = new System.Drawing.Point(639, 201);
+            this.eta.Name = "eta";
+            this.eta.Size = new System.Drawing.Size(100, 22);
+            this.eta.TabIndex = 7;
+            // 
+            // dpeers
+            // 
+            this.dpeers.Location = new System.Drawing.Point(639, 238);
+            this.dpeers.Name = "dpeers";
+            this.dpeers.Size = new System.Drawing.Size(100, 22);
+            this.dpeers.TabIndex = 8;
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 406);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(776, 32);
+            this.progress.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Peers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Estimated Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "AVG Download";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(520, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Current Download";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Output";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(338, 377);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Download Progress";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(625, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Join us over at Discord!";
+            // 
+            // discordLink
+            // 
+            this.discordLink.AutoSize = true;
+            this.discordLink.Location = new System.Drawing.Point(625, 25);
+            this.discordLink.Name = "discordLink";
+            this.discordLink.Size = new System.Drawing.Size(141, 16);
+            this.discordLink.TabIndex = 17;
+            this.discordLink.TabStop = true;
+            this.discordLink.Text = "discord.gg/r5reloaded";
+            this.discordLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.discordLink_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.discordLink);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.dpeers);
+            this.Controls.Add(this.eta);
+            this.Controls.Add(this.downRateAvg);
+            this.Controls.Add(this.downRate);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox downRate;
+        private System.Windows.Forms.TextBox downRateAvg;
+        private System.Windows.Forms.TextBox eta;
+        private System.Windows.Forms.TextBox dpeers;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel discordLink;
+    }
+}
+
---
+++ R5R_Installer/Form1.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using NuGet.Packaging;
+using SuRGeoNix;
+using SuRGeoNix.BitSwarmLib;
+using SuRGeoNix.BitSwarmLib.BEP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace R5R_Installer
+{
+    public partial class Form1 : Form
+    {
+
+        static Torrent torrent;
+        static BitSwarm bitSwarm;
+        static Options opts;
+        string Ddirectory;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinkLabel.Link link = new LinkLabel.Link();
+            link.LinkData = "https://www.discord.gg/r5reloaded";
+            discordLink.Links.Add(link);    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Ddirectory = dialog.SelectedPath;
+                textBox1.Text = Ddirectory;
+            }
+        }
+        bool hasStarted = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            opts = new Options();
+
+            opts.FolderComplete = Ddirectory;
+            opts.FolderIncomplete = Ddirectory + "/R5R-Downloading-Temp/";
+            opts.MaxTotalConnections = 120;
+            opts.MaxNewConnections = 300;
+            opts.PeersFromTracker = -1;
+            opts.BlockRequests = 500;
+            opts.ConnectionTimeout = 4000;
+            opts.HandshakeTimeout = 4000;
+            opts.PieceTimeout = 5500;
+            opts.Verbosity = 0;
+            opts.LogDHT = false;
+            opts.LogStats = false;
+            opts.LogTracker = false;
+            opts.LogPeer = false;
+
+            bitSwarm = new BitSwarm(opts);
+            bitSwarm.StatsUpdated += BitSwarm_StatsUpdated;
+            bitSwarm.MetadataReceived += BitSwarm_MetadataReceived;
+            bitSwarm.StatusChanged += BitSwarm_StatusChanged;
+
+            if (!hasStarted)
+            {
+                bitSwarm.Open("magnet:?xt=urn:btih:KCQJQT6DV2V4XWCOKCRM4EJELRLHQKI5&dn=R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM&tr=udp%3A%2F%2Fwambo.club%3A1337%2Fannounce");
+                bitSwarm.Start();
+            }
+            else;
+        }
+
+        private void BitSwarm_MetadataReceived(object source, BitSwarm.MetadataReceivedArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => BitSwarm_MetadataReceived(source, e)));
+                return;
+            }
+            else
+            {
+                torrent = e.Torrent;
+                output.Text += bitSwarm.DumpTorrent().Replace("\n", "\r\n");
+            }
+        }
+        private void BitSwarm_StatusChanged(object source, BitSwarm.StatusChangedArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => BitSwarm_StatusChanged(this, e)));
+                return;
+            }
+
+            button1.Text = "Start";
+
+            if (e.Status == 0)
+            {
+                string fileName = "";
+                if (torrent.file.name != null) fileName = torrent.file.name;
+                if (torrent != null) { torrent.Dispose(); torrent = null; }
+
+                MessageBox.Show("Downloaded successfully!\r\n" + "Starting detours and scripts install.");
+                StartR5RDetoursAndScripts();
+            }
+            else
+            {
+
+                if (e.Status == 2)
+                {
+                    output.Text += "\r\n\r\n" + "An error occurred :(\r\n\t" + e.ErrorMsg;
+                    MessageBox.Show("An error occured :( \r\n" + e.ErrorMsg);
+                }
+            }
+
+            if (torrent != null) torrent.Dispose();
+        }
+        private void BitSwarm_StatsUpdated(object source, BitSwarm.StatsUpdatedArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => BitSwarm_StatsUpdated(source, e)));
+                return;
+            }
+            else
+            {
+                downRate.Text = String.Format("{0:n0}", (e.Stats.DownRate / 1024)) + " KB/s";
+                downRateAvg.Text = String.Format("{0:n0}", (e.Stats.AvgRate / 1024)) + " KB/s";
+                eta.Text = TimeSpan.FromSeconds((e.Stats.ETA + e.Stats.AvgETA) / 2).ToString(@"hh\:mm\:ss");
+                dpeers.Text = e.Stats.PeersTotal.ToString();
+
+                if (torrent != null && torrent.data.totalSize != 0)
+                    progress.Value = e.Stats.Progress;
+            }
+
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void StartR5RDetoursAndScripts()
+        {
+            if(Directory.Exists(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/"))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    string rstring = RandomString(10);
+                    WebClient scriptsDownloader = new WebClient();
+                    string dString = scriptsDownloader.DownloadString("https://api.r5rmodmanager.com/v1.php?data=detours");
+                    scriptsDownloader.DownloadFile(new Uri(dString), Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/detours-" + rstring + ".zip");
+                
+                Thread.Sleep(1000);
+
+                    var dExtract = ZipFile.Open(Ddirectory+ "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/detours-" + rstring + ".zip", ZipArchiveMode.Read);
+                    ZipArchiveExtensions.ExtractToDirectory(dExtract, Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/", true);
+                    dExtract.Dispose();
+                    File.Delete(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/detours-" + rstring + ".zip");
+                    Thread.Sleep(1000);
+
+                    if (!Directory.Exists(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform"))
+                    {
+                        Directory.CreateDirectory(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform");
+                    }
+                    scriptsDownloader.DownloadFile(new Uri("https://github.com/Mauler125/scripts_r5/archive/refs/heads/S3_N1094.zip"), Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/newscripts.zip");
+
+                    Thread.Sleep(1000);
+
+                    if (Directory.Exists(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/scripts"))
+                    {
+                        Directory.Delete(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/scripts", true);
+                    }
+
+                    var scriptszip = ZipFile.Open(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/newscripts.zip", ZipArchiveMode.Read);
+                    ZipArchiveExtensions.ExtractToDirectory(scriptszip, Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/", true);
+                    scriptszip.Dispose();
+                    Thread.Sleep(1000);
+
+                    File.Delete(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/newscripts.zip");
+
+                    Directory.Move(Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/scripts_r5-S3_N1094", Ddirectory + "/R5pc_r5launch_N1094_CL456479_2019_10_30_05_20_PM/platform/scripts");
+
+                });
+            }
+        }
+
+        private void discordLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+    }
+    public static class ZipArchiveExtensions
+    {
+        public static void ExtractToDirectory(this ZipArchive archive, string destinationDirectoryName, bool overwrite)
+        {
+            if (!overwrite)
+            {
+                archive.ExtractToDirectory(destinationDirectoryName);
+                return;
+            }
+            foreach (ZipArchiveEntry file in archive.Entries)
+            {
+                string completeFileName = Path.Combine(destinationDirectoryName, file.FullName);
+                string directory = Path.GetDirectoryName(completeFileName);
+
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+
+                if (file.Name != "")
+                    file.ExtractToFile(completeFileName, true);
+            }
+        }
+    }
+}
